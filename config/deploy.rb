8361fdb22150cf782e825f457eb8c8b10db9708b
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2019-10-06 20:55:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-sapce'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:TsubasaNagahashi/chat-space.git'
