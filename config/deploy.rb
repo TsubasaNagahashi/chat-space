3c590369f72e5f9fbcfab0094a5ab87a072f3363
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2019-10-06 20:33:49 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:TsubasaNagahashi/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
