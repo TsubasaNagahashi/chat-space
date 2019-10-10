16985d212760e54711410a5020a834fa4fcc9a5e
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2019-10-10 18:39:29 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,75 +1,77 @@
-$(function(){
- 
-  function buildHTML(message){
+$(document).on("turbolinks:load", function() {
+  $(function(){
+  
+    function buildHTML(message){
 
-    var image = message.image? `<img src="${message.image}" class="message_text"></img>` : "" ;
+      var image = message.image? `<img src="${message.image}" class="message_text"></img>` : "" ;
 
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="message_upper-info">
-                    <div class="message_upper-info_talker">
-                      ${message.user_name}
+      var html = `<div class="message" data-id="${message.id}">
+                    <div class="message_upper-info">
+                      <div class="message_upper-info_talker">
+                        ${message.user_name}
+                      </div>
+                      <div class="message_upper-info_date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class="message_upper-info_date">
-                      ${message.created_at}
+                    <div class="message_text">
+                      <p class="lower-message__content">
+                        ${message.content}   
+                      </p>
+                        ${image}
                     </div>
-                  </div>
-                  <div class="message_text">
-                    <p class="lower-message__content">
-                      ${message.content}   
-                    </p>
-                      ${image}
-                  </div>
-                </div>`
-    return html;
-  }
+                  </div>`
+      return html;
+    }
 
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $("#new_message")[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    })
-    .fail(function(){
-      alert('error');
-    })
-    .always(function(){
-      $('.sentbtn').prop('disabled', false);
-    })
-  })
-
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      last_message_id = $('.message:last').data("id");
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
       $.ajax({
-        url: "api/messages",
-        type: 'GET',
+        url: url,
+        type: "POST",
+        data: formData,
         dataType: 'json',
-        data: {id: last_message_id}
+        processData: false,
+        contentType: false
       })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function (message) {
-        insertHTML = buildHTML(message);
-        $('.messages').append(insertHTML); 
-        $('div').animate({scrollTop: $('.messages').height()})
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $("#new_message")[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      })
+      .fail(function(){
+        alert('error');
+      })
+      .always(function(){
+        $('.sentbtn').prop('disabled', false);
+      })
+    })
+
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        last_message_id = $('.message:last').data("id");
+        $.ajax({
+          url: "api/messages",
+          type: 'GET',
+          dataType: 'json',
+          data: {id: last_message_id}
         })
-      })
-      .fail(function() {
-        alert('更新に失敗しました');
-      });
-    };
-  } 
-  setInterval(reloadMessages, 5000); 
-});
+        .done(function(messages) {
+          var insertHTML = '';
+          messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML); 
+          $('div').animate({scrollTop: $('.messages').height()})
+          })
+        })
+        .fail(function() {
+          alert('更新に失敗しました');
+        });
+      };
+    } 
+    setInterval(reloadMessages, 5000); 
+  });
+})
